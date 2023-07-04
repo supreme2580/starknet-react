8e78dee02a6b3199cb3cdbdbba403ab621eb2362
fracek <fracek@users.noreply.github.com>
2023-07-04 08:28:02 +0000

    deploy: 3f02c76f0765b078526b6a6fd84df329516b5a15

---
+++ _next/static/jydbW-gD9wCLr49i3saF6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-bc1aa1767a2e5d3b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
